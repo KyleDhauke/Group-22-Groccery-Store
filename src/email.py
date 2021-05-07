7908b056aa96e2584c6968511fb8f27ced4ac811
yy-wyy <57929702+yy-wyy@users.noreply.github.com>
2021-05-07 21:07:12 +0800

    transcript

--- src/email.py
+++ src/email.py
@@ -3,7 +3,7 @@ from flask_mail import Message
 from flask import render_template, current_app
 from src import mail
 
-def send_email(to, subject, template,user, **kwargs):
+def send_email(to, subject, template, user, **kwargs):
     app = current_app._get_current_object()
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
